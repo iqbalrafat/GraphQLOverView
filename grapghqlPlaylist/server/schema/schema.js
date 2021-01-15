1978138c09550aa37dd61a5a6a38d2f6416c9e00
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-01-15 01:44:32 -0700

    [GrapghQL] - extended Schema and two way Query

--- grapghqlPlaylist/server/schema/schema.js
+++ grapghqlPlaylist/server/schema/schema.js
@@ -2,13 +2,16 @@ const graphql =require('graphql');
 const _=require('lodash');
 
 const{GraphQLObjectType,GraphQLString, GraphQLSchema,
-   GraphQLID, GraphQLInt} = graphql;
+   GraphQLID, GraphQLInt,GraphQLList} = graphql;
 
 //dummy Data
 var books =[
   {name: 'Name of the Wind',genre:'Fantasy',id:'1',authorId:'1'},
   {name: 'The Long Earth',genre:'Sci-Fi',id:'2',authorId:'2'},
   {name: 'Ertugral The legend',genre:'History',id:'3',authorId:'3'},
+  {name: 'The Hero of Ages',genre:'Fantasy',id:'4',authorId:'2'},
+  {name: 'The Color of Magic',genre:'Fantasy',id:'5',authorId:'3'},
+  {name: 'The Light Fantastic',genre:'Fantasy',id:'6',authorId:'3'},
 ];
 var authors = [
   {name:'Patrick Rothfuss',age:44,id:'1'},
@@ -27,8 +30,7 @@ const BookType = new GraphQLObjectType({
       resolve(parent,args){
         console.log(parent);
         return _.find(authors,{id:parent.authorId});
-      }
-      
+      }      
     }
   })
 });
@@ -37,7 +39,13 @@ const AuthorType = new GraphQLObjectType({
   fields:()=>({
     id:{type:GraphQLID},
     name:{type:GraphQLString},
-    age:{type:GraphQLInt}
+    age:{type:GraphQLInt},
+    books:{
+      type: new GraphQLList(BookType),
+      resolve(parent,args){
+      return _.filter(books,{authorId:parent.id})  // we apply filter on books array to find authorId 
+    }
+    }
   })
 });
 
